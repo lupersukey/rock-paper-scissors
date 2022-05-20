8ef43412e0a302b4e41438d52a916823683a79de
lupersukey <luke.staylor96@gmail.com>
2022-05-20 18:22:27 -0400

    added playRound() function that takes player selection and computer selection and decides a winner

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 
 let computerChoice = computerPlay();
+let playerChoice = "Rock"
 
 function computerPlay(){
     let options = ["Rock", "Paper", "Scissors"];
     return choice = options[Math.floor(Math.random() * 3)];
 }
 
+function playRound(playerChoice, computerChoice){
+    if (playerChoice == "Rock" && computerChoice == "Scissors"){
+        return "You win this round!";
+    } else if (playerChoice == "Rock" && computerChoice == "Paper"){
+        return "You lose this round!";
+    }
+
+    if (playerChoice == "Scissors" && computerChoice == "Paper"){
+        return "You win this round!";
+    } else if (playerChoice == "Scissors" && computerChoice == "Rock"){
+        return "You lose this round!";
+    }
+
+    if (playerChoice == "Paper" && computerChoice == "Rock"){
+        return "You win this round!";
+    } else if (playerChoice == "Paper" && computerChoice == "Scissors"){
+        return "You lose this round!";
+    }
+
+    if (playerChoice === computerChoice){
+        return "This round is a draw, no points awarded!";
+    }
+}
+
+console.clear();
+console.log(playRound(playerChoice, computerChoice));
