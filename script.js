afacfb0b69c8b27499ba4aad84d7cc52bbb7f49c
lupersukey <luke.staylor96@gmail.com>
2022-05-20 18:42:54 -0400

    added the ability to keep score between the player and computer and once one player hits 5, the game is over and winner is decided.

--- script.js
+++ script.js
@@ -1,42 +1,62 @@
 
+let playerScore = 0;
+let computerScore = 0;
+
+while (playerScore < 5 && computerScore < 5){
 let computerChoice = computerPlay();
 let playerChoice = playerPlay();
 
-function playerPlay(){
-    let choice = prompt("Rock, Paper or Scissors?").toLowerCase();
-    return choice.charAt(0).toUpperCase() + choice.slice(1);
+    function playerPlay(){
+        let choice = prompt("Rock, Paper or Scissors?").toLowerCase();
+        return choice.charAt(0).toUpperCase() + choice.slice(1);
+    }
+
+    function computerPlay(){
+        let options = ["Rock", "Paper", "Scissors"];
+        return choice = options[Math.floor(Math.random() * 3)];
+    }
+
+    function playRound(playerChoice, computerChoice){
+        if (playerChoice == "Rock" && computerChoice == "Scissors"){
+            playerScore ++;
+            return "You win this round!";
+        } else if (playerChoice == "Rock" && computerChoice == "Paper"){
+            computerScore ++;
+            return "You lose this round!";
+        }
+
+        if (playerChoice == "Scissors" && computerChoice == "Paper"){
+            playerScore ++;
+            return "You win this round!";
+        } else if (playerChoice == "Scissors" && computerChoice == "Rock"){
+            computerScore ++;
+            return "You lose this round!";
+        }
+
+        if (playerChoice == "Paper" && computerChoice == "Rock"){
+            playerScore ++;
+            return "You win this round!";
+        } else if (playerChoice == "Paper" && computerChoice == "Scissors"){
+            computerScore ++;
+            return "You lose this round!";
+        }
+
+        if (playerChoice === computerChoice){
+            return "This round is a draw, no points awarded!";
+        }
+    }
+    console.clear();
+    console.log("You chose: " + playerChoice);
+    console.log("Computer chose: " + computerChoice);
+    console.log(playRound(playerChoice, computerChoice));
+    console.log("---S C O R E S---");
+    console.log("You: " + playerScore + " Computer: " + computerScore);
 }
 
-function computerPlay(){
-    let options = ["Rock", "Paper", "Scissors"];
-    return choice = options[Math.floor(Math.random() * 3)];
+
+
+if (playerScore == 5){
+    console.log("Well done, you defeated the computer!");
+} else{
+    console.log("Unlucky, the computer has defeated you!");
 }
-
-function playRound(playerChoice, computerChoice){
-    if (playerChoice == "Rock" && computerChoice == "Scissors"){
-        return "You win this round!";
-    } else if (playerChoice == "Rock" && computerChoice == "Paper"){
-        return "You lose this round!";
-    }
-
-    if (playerChoice == "Scissors" && computerChoice == "Paper"){
-        return "You win this round!";
-    } else if (playerChoice == "Scissors" && computerChoice == "Rock"){
-        return "You lose this round!";
-    }
-
-    if (playerChoice == "Paper" && computerChoice == "Rock"){
-        return "You win this round!";
-    } else if (playerChoice == "Paper" && computerChoice == "Scissors"){
-        return "You lose this round!";
-    }
-
-    if (playerChoice === computerChoice){
-        return "This round is a draw, no points awarded!";
-    }
-}
-
-console.clear();
-console.log("You chose: " + playerChoice);
-console.log("Computer chose: " + computerChoice);
-console.log(playRound(playerChoice, computerChoice));
