c741af2ad4c36586dcf8bdc56594ec1c199b8c2d
lupersukey <luke.staylor96@gmail.com>
2022-05-20 18:12:06 -0400

    added function that randomly returns rock paper or scissors as the computers choice

---
+++ script.js
@@ -0,0 +1,8 @@
+
+let computerChoice = computerPlay();
+
+function computerPlay(){
+    let options = ["Rock", "Paper", "Scissors"];
+    return choice = options[Math.floor(Math.random() * 3)];
+}
+
